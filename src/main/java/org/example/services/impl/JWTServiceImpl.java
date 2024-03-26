b128f6187fa3b490b6c704750cd24eb62dddc452
Don-Tun <val.neerg@gmail.com>
2024-03-26 12:29:32 +0100

    token life-time

--- src/main/java/org/example/services/impl/JWTServiceImpl.java
+++ src/main/java/org/example/services/impl/JWTServiceImpl.java
@@ -21,7 +21,7 @@ public class JWTServiceImpl implements JWTService {
     public String generateToken(UserDetails userDetails){
         return Jwts.builder().setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 10))// * 30)) //milliseconds * seconds * minutes
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 10 * 30)) //milliseconds * seconds * minutes
                 .signWith(getSigninKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
