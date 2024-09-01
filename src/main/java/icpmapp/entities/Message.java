493ba568b606009579926ed52c3068ce867c6560
Andrea Burattin <andrea.burattin@gmail.com>
2024-09-01 15:20:34 +0200

    Added message entity

---
+++ src/main/java/icpmapp/entities/Message.java
@@ -0,0 +1,24 @@
+package icpmapp.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Table(name = "messages")
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author")
+    private User author;
+
+    private String path;
+    private String text;
+    private LocalDateTime creationTime;
+}
